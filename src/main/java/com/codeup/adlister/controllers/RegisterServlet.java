a69965f33d69a07516e434246ffa94ab881dda78
Juan Garcia <garcia.d.juan14@gmail.com>
2021-07-18 22:11:22 -0500

    adeed regex an more messages

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Helper;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,45 +26,41 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
-        boolean inputHasErrors =
-                username.isEmpty()
-                        || email.isEmpty()
-                        || !email.contains("@")
-                        || password.isEmpty()
-                        || (!password.equals(passwordConfirmation));
+        // this is being called from the helper java in the util which is checking if the username and email are valid options
+        boolean validUsername = Helper.validUsername(username);
+        boolean validEmail = Helper.validEmail(email);
 
-        if (inputHasErrors) {
-            response.sendRedirect("/register");
-            return;
-        }
-        System.out.println("this is somewhat working");
+        // this is being called from the helper java in the util which is checking if the email and username have not being used.
+       boolean usernameNotExist = Helper.usernameNotExist(username);
+       boolean emailNotExist = Helper.emailNotExist(email);
 
-        boolean usernameNotExist = false;
-        boolean emailNotExist = false;
-        boolean userConflicts = false;
+       // this are messages for valid usernames and emails
+        String usernameValid = "<script>alert('This Username is not a valid option, try again');</script>";
+        String emailValid = "<script>alert('This Email is not a valid option, try again');</script>";
+        String emailNUsernameValid = "<script>alert('The username and email already exist with another user, try again');</script>";
 
-        User nameTest = DaoFactory.getUsersDao().findByUsername(username);
-        User emailTest = DaoFactory.getUsersDao().findByEmail(email);
-
-        if (nameTest == null) {
-            usernameNotExist = true;
-        }
-        if (emailTest == null) {
-            emailNotExist = true;
-        }
-        if (usernameNotExist && emailNotExist) {
-            userConflicts = true;
+        // this is checking if our email and username are valid
+        if(!validUsername && !validEmail){
+            request.setAttribute("emailNUsernameValid", emailNUsernameValid);
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else if (!validEmail){
+            request.setAttribute("username", username);
+            request.setAttribute("emailValid", emailValid);
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else if(!validUsername){
+            request.setAttribute("email", email);
+            request.setAttribute("usernameValid", usernameValid);
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
         }
 
+
+       // this are our alert messages for existing email and users
         String usernameExistMessage = "<script>alert('The Username already exists, try again');</script>";
         String emailExistMessage = "<script>alert('The email already exist with another user, try again');</script>";
         String emaiNUsernameExistMessage = "<script>alert('The username and email already exist with another user, try again');</script>";
 
-        if (userConflicts) {
-            User user = new User(username, email, password);
-            DaoFactory.getUsersDao().insert(user);
-            response.sendRedirect("/login");
-        } else if (!usernameNotExist && !emailNotExist) {
+        // this is checking if the email an the username already exist on out datatabase
+        if (!usernameNotExist && !emailNotExist) {
             request.setAttribute("emaiNUsernameExistMessage", emaiNUsernameExistMessage);
             request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
         } else if (!emailNotExist) {
@@ -74,5 +72,19 @@ public class RegisterServlet extends HttpServlet {
             request.setAttribute("usernameExistMessage", usernameExistMessage);
             request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
         }
+
+        boolean inputHasErrors =
+                username.isEmpty()
+                        || email.isEmpty()
+                        || password.isEmpty()
+                        || (!password.equals(passwordConfirmation));
+
+        if (inputHasErrors) {
+            response.sendRedirect("/register");
+            return;
+        }
+        User user = new User(username, email, password);
+        DaoFactory.getUsersDao().insert(user);
+        response.sendRedirect("/login");
     }
 }
--- src/main/java/com/codeup/adlister/util/Helper.java
+++ src/main/java/com/codeup/adlister/util/Helper.java
@@ -1,5 +1,11 @@
 package com.codeup.adlister.util;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Helper {
 
     public static String formatReturnAddress(StringBuffer url, String queryString)
@@ -15,5 +21,53 @@ public class Helper {
             }
         }
         return address;
+    }
+
+    public static boolean validUsername(String username){
+        // regex to check valid username
+        String regexUsername = "^[A-Za-z]\\w{5,29}$";
+
+        // this is compiling the regex
+        Pattern pattern = Pattern.compile(regexUsername);
+
+        // if the username is empty it returns false
+        if(username == null){
+            return false;
+        }
+
+        // pattern class the matcher method which is used to find matching between the input username and the regular expression
+        Matcher matcher = pattern.matcher(username);
+
+        //returns if the username matches the regex
+        return matcher.matches();
+    }
+
+    public static boolean validEmail(String email){
+        // regex to check valid email
+        String regexEmail = "^[a-zA-Z0-9_+&*-]+(?:\\.+[a-zA-Z0-9_+&*-]+)*@+(?:[a-zA-Z0-9-]+\\.)+[a-z+A-Z]{2,7}$";
+
+        // this is compiling the regex
+        Pattern pattern = Pattern.compile(regexEmail);
+
+        // if the email is empty it returns false
+        if (email == null){
+            return false;
+        }
+
+        // pattern class the matcher method which is used to find matching between the input email and the regular expression
+        Matcher matcher = pattern.matcher(email);
+
+        //returns if the email matches the regex
+        return matcher.matches();
+    }
+
+    public static boolean usernameNotExist(String username){
+        User nameTest = DaoFactory.getUsersDao().findByUsername(username);
+        return nameTest == null;
+    }
+
+    public static boolean emailNotExist(String email){
+        User emailTest = DaoFactory.getUsersDao().findByUsername(email);
+        return emailTest == null;
     }
 }
